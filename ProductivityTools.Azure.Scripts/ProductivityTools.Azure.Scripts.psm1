ccb4f3874d58b5589191fe249e90eb4f44963cc2
Pawel Wujczyk <pwujczyk@hotmail.com>
2021-04-27 19:09:51 +0200

    add file changed

--- ProductivityTools.Azure.Scripts/ProductivityTools.Azure.Scripts.psm1
+++ ProductivityTools.Azure.Scripts/ProductivityTools.Azure.Scripts.psm1
@@ -6,20 +6,22 @@ function GetConfiguration{
 
 	$location = Get-MasterConfiguration $("$Profile"+":Location")
 	$resourceGroup = Get-MasterConfiguration $("$Profile"+":ResourceGroup")
+	$storageName = Get-MasterConfiguration $("$Profile"+":StorageName")
+	$imagesContainerName = Get-MasterConfiguration $("$Profile"+":ImagesContainerName")
 
 	$config = New-Object -TypeName psobject 
-	$config | Add-Member -MemberType NoteProperty -Name Location -Value $location
+#	$config | Add-Member -MemberType NoteProperty -Name Location -Value $location
 	$config | Add-Member -MemberType NoteProperty -Name ResourceGroup -Value $resourceGroup
-	$config | Add-Member -MemberType NoteProperty -Name StorageName -Value "ptblogstorage"
-	$config | Add-Member -MemberType NoteProperty -Name SkuName -Value "Standard_RAGRS"
-	$config | Add-Member -MemberType NoteProperty -Name ImagesContainerName -Value "images"
+	$config | Add-Member -MemberType NoteProperty -Name StorageName -Value $storageName
+	# $config | Add-Member -MemberType NoteProperty -Name SkuName -Value "Standard_RAGRS"
+	$config | Add-Member -MemberType NoteProperty -Name ImagesContainerName -Value $imagesContainerName
 
 
-	$config | Add-Member -MemberType NoteProperty -Name CdnProfileName -Value "productivitycdnprofile" 
-	$config | Add-Member -MemberType NoteProperty -Name CdnSku -Value "Standard_Akamai" 
-	$config | Add-Member -MemberType NoteProperty -Name CdnEndpointName -Value "ptblog" 
-	$config | Add-Member -MemberType NoteProperty -Name CdnHostname -Value "cdn.productivitytools.tech" 
-	$config | Add-Member -MemberType NoteProperty -Name CustomDomainName  -Value "cdn3" 
+	# $config | Add-Member -MemberType NoteProperty -Name CdnProfileName -Value "productivitycdnprofile" 
+	# $config | Add-Member -MemberType NoteProperty -Name CdnSku -Value "Standard_Akamai" 
+	# $config | Add-Member -MemberType NoteProperty -Name CdnEndpointName -Value "ptblog" 
+	# $config | Add-Member -MemberType NoteProperty -Name CdnHostname -Value "cdn.productivitytools.tech" 
+	# $config | Add-Member -MemberType NoteProperty -Name CustomDomainName  -Value "cdn3" 
 	return $config
 }
 
@@ -58,9 +60,7 @@ function Push-FileToAzureBlobStorage{
 	
 	
 	$config=GetConfiguration $Profile
-	$context=GetContext
-	$blob=Set-AzStorageBlobContent -File $Path -Container $config.ImagesContainerName -Blob $DestinationFileName  -Context $context -Force 
+	$context=GetContext $Profile
+	$blob=Set-AzStorageBlobContent -File $Path -Container $config.ImagesContainerName -Blob $DestinationFileName -Context $context -Force 
 	return $blob
 }
-#clear
-#Addfile "c:\Diagram.png"
